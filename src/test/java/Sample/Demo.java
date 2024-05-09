3a61a89e49f2001c2655e0003837c6189a7b0acd
Ajiket97 <ajitshingnare02@gmail.com>
2024-05-09 12:40:49 +0530

    m

--- src/test/java/Sample/Demo.java
+++ src/test/java/Sample/Demo.java
@@ -5,6 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("This is demo class");
 		System.out.println("hi i am ajit");
+		System.out.println("My 2nd PR request");
 	}
 
 }
