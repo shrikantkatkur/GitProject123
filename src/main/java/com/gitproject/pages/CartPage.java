c5a6a1e46984eebb5172bfb6696051ccf050b8fe
AtishZoting <atish.zoting@gmail.com>
2024-06-18 18:33:30 +0530

    added

--- src/main/java/com/gitproject/pages/CartPage.java
+++ src/main/java/com/gitproject/pages/CartPage.java
@@ -2,7 +2,22 @@ package com.gitproject.pages;
 
 public class CartPage {
 
+	 int i=10;
+	
 	public static void main(String[] args) {
-		System.out.println("this is cart page");
+	
+		CartPage t1= new CartPage();
+		CartPage t2= new CartPage();
+		CartPage t3= new CartPage();
+		
+		t1.i= t1.i+5;
+		t2.i=t1.i+10;
+		
+		
+		
+		
+		System.out.println(t1.i);
+		System.out.println(t2.i);
+		System.out.println(t3.i);
 	}
 }
